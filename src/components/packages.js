d951a75031f8e181ae480e88339f56999ac3ace9
devnirajc <nirajd327@gmail.com>
2021-09-04 01:09:27 +0530

    package page added

--- src/components/packages.js
+++ src/components/packages.js
@@ -1,7 +1,50 @@
 import React from 'react'
 
-function Packages() {
-    return;
-}
+const Packages = () => (
+    <div class="container-fluid sec_padding four">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="text-center sec_head wow fadeInDown">
+                        Select Packages
+                    </h3>
+                    <p class="text-center sec_sub_head">
+                        We have 3 packages to fulfill your need.
+                    </p>
+                    <div class="border_btm"></div>
+                </div>
+            </div>
+            <div class="row ml-0">
+                <div class="happy-customer wow zoomIn">
+                    <div class="wide_range_inner_value">
+                        <h1>50K</h1>
+                        <p>Customers</p>
+                    </div>
+                </div>
+                <div class="happy-customer wow zoomIn">
+                    <div class="wide_range_inner_value">
+                        <div class="icon"><i class="fa fa-car"></i></div>
+                        <h1>43</h1>
+                        <p>Car Brands</p>
+                    </div>
+                </div>
+                <div class="happy-customer wow zoomIn">
+                    <div class="wide_range_inner_value">
+                        <div class="icon"><i class="fa fa-globe"></i></div>
+                        <h1>7</h1>
+                        <p>Cities</p>
+                    </div>
+                </div>
+                <div class="happy-customer wow zoomIn">
+                    <div class="wide_range_inner_value">
+                        <div class="icon"><i class="fa fa-tint"></i></div>
+                        <h1>10.4</h1>
+                        <p>Million Litres of Water Saved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+)
 
 export default Packages
--- src/containers/home.js
+++ src/containers/home.js
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import Header from './../shared/components/header';
 import CarService from './../components/car-service';
 import Categories from './../components/categories';
+import Packages from './../components/packages';
 
 const Home = () => (
     <Fragment>
         <Header />
         <CarService />
         <Categories />
+        <Packages />
     </Fragment>
 )
 
