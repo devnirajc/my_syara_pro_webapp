cea3c614cc2122b7c3304d60d84ad68c35263ece
devnirajc <nirajd327@gmail.com>
2021-09-05 14:58:05 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import './assets/css/header.css';
 import './assets/css/login.css';
 import './assets/css/responsive.css';
 
-import Home from './containers/home';
-import Subscription from './containers/subscription';
-
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <Subscription />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
+import Routes from './routes';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/css/style.css';
+import './assets/css/header.css';
+import './assets/css/login.css';
+import './assets/css/responsive.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Routes/>, document.getElementById('root'))
